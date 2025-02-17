5e07551c169918a47a117ee7ada5485ad178467b
khannsameer <sk8717108@gmail.com>
2025-02-17 22:32:36 +0530

    add item.js file

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -2,9 +2,9 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import { Outlet } from "react-router-dom";
-import FetchItems from "../components/fetchItems";
 import { useSelector } from "react-redux";
 import LoadingSpinner from "../components/LoadingSpinner";
+import FetchItems from "../components/FetchItems";
 
 function App() {
   const fetchStatus = useSelector((store) => store.fetchStatus);
